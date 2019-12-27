201599555beac157863802d42102b47c15dbef8c
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-12-27 20:55:38 +0800

    Add email service

--- functions/welcome.js
+++ functions/welcome.js
@@ -1,13 +1,28 @@
+const sgMail = require("@sendgrid/mail");
+
 exports.handler = function(event, context, callback) {
-  const {queryStringParameters} = event;
+  try {
+    const {queryStringParameters} = event;
+    const {email} = queryStringParameters;
 
-  console.log(event);
-  console.log(context);
-  console.log(queryStringParameters);
+    console.log(email);
 
-  const error = null;
-  callback(error, {
-    statusCode: 200,
-    body: "Hello, world"
-  });
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const message = {
+      to: email,
+      from: "test@example.com",
+      subject: "My first email",
+      text: "This is my first email"
+    };
+
+    sgMail.send(message);
+
+    callback(null, {
+      statusCode: 200,
+      body: "Welcome email was sent"
+    });
+  } catch (error) {
+    callback(error);
+  }
 };
