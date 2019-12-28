4f5209839e7aa1c5d3f90266dadfac4137c66936
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-12-28 17:23:59 +0800

    Add function for form submission

---
+++ functions/submission-created.js
@@ -0,0 +1,28 @@
+const sgMail = require("@sendgrid/mail");
+const templateId = "d-56e970c0a6f740aea7ba6744f806987d";
+
+exports.handler = function(event, context, callback) {
+  try {
+    const {queryStringParameters} = event;
+    const {email} = queryStringParameters;
+
+    console.log(event);
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+    const message = {
+      to: email,
+      from: {name: "Jumpstart", email: "info@jumpstart.sh"},
+      template_id: templateId
+    };
+
+    sgMail.send(message);
+
+    callback(null, {
+      statusCode: 200,
+      body: "Email has been delivered"
+    });
+  } catch (error) {
+    callback(error);
+  }
+};
