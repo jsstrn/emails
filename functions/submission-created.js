490fbb8ca2e9b959e8e322f5ac5b3a217a6bcd4d
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-12-28 17:29:10 +0800

    Use dummy email

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,15 +3,12 @@ const templateId = "d-56e970c0a6f740aea7ba6744f806987d";
 
 exports.handler = function(event, context, callback) {
   try {
-    const {queryStringParameters} = event;
-    const {email} = queryStringParameters;
-
     console.log(event);
 
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
     const message = {
-      to: email,
+      to: "test@example.com",
       from: {name: "Jumpstart", email: "info@jumpstart.sh"},
       template_id: templateId
     };
